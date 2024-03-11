d4af6cde761decb4bf70f3ee599e2a12b624d477
anushmashaiju <anushma2015@gmail.com>
2024-03-11 10:33:51 +0530

    for hosting

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // CORS setup
 app.use(cors({
-  origin:["https://localconnectsocialmedia-appfe.onrender.com","http://localhost:3000"]
+  origin:["https://localconnectprojectfeclient.onrender.com","http://localhost:3000"]
 }));
 /*app.use(cors({
   origin: ["http://localhost:3000"],
